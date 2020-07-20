7390fdd44b548dada5784b32fac35f6efcc09e43
roikatz16 <kroi@post.bgu.ac.il>
2020-07-20 14:30:02 +0300

    about

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,24 +1,12 @@
 <template>
   <div>
-    <b-container class="container">
-      <!-- Section: Team v.1 -->
-      <section class="team-section text-center my-5">
-        <!-- Section heading -->
-        <h2 class="h1-responsive font-weight-bold my-3">Michal & Roi Team</h2>
-        <!-- Section description -->
-        <p
-          class="grey-text w-responsive mx-auto mb-5"
-          style="font-size: 1.2em;"
-        >
-          Enjoy our amazing website. <br/>
-          
-        </p>
-
-        <!-- Grid row -->
-        <div class="row">
-          <!-- Grid column -->
-          <div class="col-lg-6 col-md-6 mb-lg-0 mb-5">
-            <div class="avatar mx-auto">
+    <b-container class="container">    
+      <section class="team-section text-center my-5">      
+        <h2><b>Michal & Roi Team</b></h2>     
+        <p> Enjoy our amazing website. <br/></p>       
+        <div class="row">       
+          <div>
+            <div>
               <!-- <img
                 src="../images/‏‏avitalImage.jpg"
                 class="rounded-circle z-depth-1"
@@ -27,18 +15,9 @@
                 height="170px"
               /> -->
             </div>
-            <h5 class="font-weight-bold mt-4 mb-6">Roi Katz</h5>
-            <p class="text-uppercase blue-text">
-            </p>
-            <p class="grey-text" style="text-align:left;font-size: 1.2em;">
-              Loves chewing on hummus chocolate chips cookies. <br />
-              In her spare time, a student of information systems engineering at
-              Ben Gurion University of the Negev. Specializes in NodeJS and
-              VueJS. <br />
-              You can contact me via gmail or facebook.
-              <br />
-            </p>
-            <ul class="list-unstyled mb-0">
+            <h5 >Roi Katz</h5>           
+         
+            <ul>
               <link
                 rel="stylesheet"
                 href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
@@ -60,29 +39,16 @@
               </a>
             </ul>
           </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-lg-6 col-md-6 mb-lg-0 mb-5">
-            <div class="avatar mx-auto">
-              <!-- <img
-                src="../images/orImage.jpeg"
-                class="rounded-circle z-depth-1"
-                alt="Sample avatar"
-                width="250px"
-                height="170px"
-              /> -->
+       
+          <div >
+            <div >
+        
             </div>
-            <h5 class="font-weight-bold mt-4 mb-6">Michal Talmor</h5>
-            <p class="text-uppercase blue-text">
+            <h5 >Michal Talmor</h5>
+            <p >
             </p>
-            <p class="grey-text" style="text-align:left;font-size: 1.2em;">
-              The worlds best hummus chocolate chips cookies baker. <br />
-              In her spare time, she is a student of information systems
-              engineering at Ben Gurion University of the Negev. Specializes in
-              NodeJS and VueJS. <br />You can contact me via gmail or facebook.
-            </p>
-            <ul class="list-unstyled mb-0">
+       
+            <ul >
               <!-- Facebook -->
               <a href="https://www.facebook.com/OrAlfasi">
                 <i
@@ -101,7 +67,7 @@
         </div>
         <!-- Grid row -->
 
-        <div class="projectStory">
+        <div>
           The website developed by us using Node.js in the back side and Vue.js in the front side. <br/>
         </div>
 
@@ -142,19 +108,14 @@ export default {
 .container {
   min-height: 400px;
 }
-// .row {
-//   padding: 10 px;
-//   align-items: center;
-// }
+
 .col {
   float: left;
   width: 50%;
   margin-bottom: 16px;
   padding: 0 8px;
 }
-.p {
-  font-size: 16px;
-}
+
 .card {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   margin: 8px;
